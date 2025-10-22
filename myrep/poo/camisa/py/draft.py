c918e8a60b8c7579dadb81aac69b634d9312199a
gustavo vieira <148218274+gustavovieiradacost@users.noreply.github.com>
2025-10-22 11:52:51 +0000

    roupa completo

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -1,17 +1,42 @@
-class Roupa:
-    def __init__(self):
-        self.__tamanho = ""
-
-    def setTamanho(self, valor):
-        tamanhos = ["PP", "P", "M", "G", "GG", "XG"]
-        if valor not in tamanhos:
-            print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
-            return
-        self.__tamanho = valor
-
-    def __str__(self):
-        return f"size: ({self.__tamanho})"
+class Camisa:
+    def _init_(self):
+        self.tamanho: str = ""
 
 
-roupa = Roupa()
+    def _str_(self) -> str:
+       if self.tamanho == "":
+        return "size: ()"
+       else:
+           return f"size: ({self.tamanho})"
 
+    def getTamanho(self) -> str:
+        return self.tamanho 
+    
+
+    def setTamanho(self, valor: str) -> None:
+        valido = ["PP", "P", "M", "G", "GG", "XG"]
+
+        if valor not in valido:
+           print(f"fail: Valor inválido, tente PP, P, M, G, GG ou XG")
+        else:
+           self.tamanho = valor
+        
+  
+def main():
+    camisa: Camisa = Camisa()      
+    
+    while True:
+
+        line = input()
+        print("$" + line)
+        args = line.split()
+
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(camisa)
+        elif args[0] == "size":
+            camisa.setTamanho(args[1])
+        else:
+            print("fail: comando inválido")
+main()
